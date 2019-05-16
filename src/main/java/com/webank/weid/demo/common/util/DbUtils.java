9545f8b402c0f98c3fb488b01a3881b45f63edec
anyspa <anyspa@126.com>
2019-05-16 17:44:50 +0800

    配置policy以json的方式访问

--- src/main/java/com/webank/weid/demo/common/util/DbUtils.java
+++ src/main/java/com/webank/weid/demo/common/util/DbUtils.java
@@ -3,11 +3,14 @@ package com.webank.weid.demo.common.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webank.weid.demo.command.BaseBean;
 import com.webank.weid.demo.command.DemoBase;
 import com.webank.weid.protocol.base.Challenge;
 import com.webank.weid.protocol.base.ClaimPolicy;
 import com.webank.weid.protocol.base.PresentationPolicyE;
+import com.webank.weid.util.JsonUtil;
 
 public class DbUtils extends DemoBase {
 
@@ -49,22 +52,19 @@ public class DbUtils extends DemoBase {
         BaseBean.print(presentationPolicyE);
         return presentationPolicyE;
     }
-
+    
     private static Map<Integer, ClaimPolicy> queryPolicy() {
         
-        ClaimPolicy claim2000000 = new ClaimPolicy();
-        claim2000000.setCptId(2000000);
-        String disclosed = DemoBase.DISCLOSURE1;
-        claim2000000.setFieldsToBeDisclosed(disclosed);
+    	Map<Integer, ClaimPolicy> policy = new HashMap<>();
+    	String jsonPolicy2000000 = DemoBase.POLICY1;
+        ClaimPolicy claim2000000 = 
+            JsonUtil.jsonStrToObj(ClaimPolicy.class, jsonPolicy2000000);
         claim2000000.setVersion(1);
-        
-        Map<Integer, ClaimPolicy> policy = new HashMap<>();
         policy.put(2000000, claim2000000);
 
-        ClaimPolicy claim2000001 = new ClaimPolicy();
-        claim2000001.setCptId(2000001);
-        String disclosed2 = DemoBase.DISCLOSURE2;
-        claim2000001.setFieldsToBeDisclosed(disclosed2);
+        String jsonPolicy2000001 = DemoBase.POLICY2;
+        ClaimPolicy claim2000001 = 
+            JsonUtil.jsonStrToObj(ClaimPolicy.class, jsonPolicy2000001); 
         claim2000001.setVersion(1);
         policy.put(2000001, claim2000001);
         return policy;
