903591356a1babc6b6e1123ca4f6dccef5ee2ea7
v_wbgyang <v_wbgyang@v_wbgyang-PC01.webank.com>
2019-05-27 15:31:19 +0800

    fix policyAndChallengeOnPush

--- src/main/java/com/webank/weid/demo/service/impl/PolicyServiceImpl.java
+++ src/main/java/com/webank/weid/demo/service/impl/PolicyServiceImpl.java
@@ -1,5 +1,8 @@
 package com.webank.weid.demo.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.webank.weid.demo.command.DbUtils;
 import com.webank.weid.protocol.base.Challenge;
 import com.webank.weid.protocol.base.PolicyAndChallenge;
@@ -8,11 +11,20 @@ import com.webank.weid.service.impl.callback.PresentationPolicyService;
 
 public class PolicyServiceImpl extends PresentationPolicyService {
     
+    public static Map<String, PresentationPolicyE> policMap = 
+        new HashMap<String, PresentationPolicyE>();
+    
+    static {
+        //initialization policy
+        PresentationPolicyE presentationPolicyE = PresentationPolicyE.create("policy1001.json");
+        policMap.put("1001", presentationPolicyE);
+    }
+    
     @Override
     public PolicyAndChallenge policyAndChallengeOnPush(String policyId, String targetWeId) {
         
         //获取presentationPolicyE
-        PresentationPolicyE presentationPolicyE = PresentationPolicyE.create(policyId);
+        PresentationPolicyE presentationPolicyE = policMap.get(policyId);
         
         //获取Challenge
         Challenge challenge = 
