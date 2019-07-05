dda4371febb5044f1ac4eb2413fe30fe892a67cc
tonychen <chenhaozx@live.com>
2019-07-05 23:36:15 +0800

    ddd back

--- command.sh
+++ command.sh
@@ -11,8 +11,8 @@ if [[ $1 != 'issuer' ]];then
   fi
 fi
 
-if [[ $1 = 'daemon' ]];then
-    java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1 >/dev/null 2>&1 &
-else
+#if [[ $1 = 'daemon' ]];then
+#    java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1 >/dev/null 2>&1 &
+#else
     java -cp "./dist/conf/:./dist/lib/*:./dist/app/*:./libs/*" com.webank.weid.demo.command.DemoCommand $1
-fi
+#fi
