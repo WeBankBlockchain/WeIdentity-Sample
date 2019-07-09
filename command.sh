1113f2cd38685fb4b2fbd07cf50b1682d3166037
tonychen <chenhaozx@live.com>
2019-07-09 19:00:18 +0800

    use double quotes

--- command.sh
+++ command.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-if [[ $1 != 'issuer' ]];then
-  if [[ $1 != 'user_agent' ]];then
-     if [[ $1 != 'verifier' ]];then
-        if [[ $1 != 'daemon' ]];then
+if [[ "$1" != "issuer" ]];then
+  if [[ "$1" != "user_agent" ]];then
+     if [[ "$1" != "verifier" ]];then
+        if [[ "$1" != "daemon" ]];then
            echo "please input the correct parameters:issuer, user_agent, verifier, daemon"
            exit 1
         fi
@@ -11,7 +11,7 @@ if [[ $1 != 'issuer' ]];then
   fi
 fi
 
-if [[ $1 = 'daemon' ]];then
+if [[ "$1" = "daemon" ]];then
     java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1 >/dev/null 2>&1 &
 else
     java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1
