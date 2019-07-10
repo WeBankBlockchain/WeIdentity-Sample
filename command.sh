c78860567b53af553c815393d392344c594e229d
anyspa <35908094+anyspa@users.noreply.github.com>
2019-07-10 09:43:02 +0800

    Update command.sh
    add echo

--- command.sh
+++ command.sh
@@ -14,5 +14,6 @@ fi
 if [[ "$1" == daemon ]];then
     java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1 >/dev/null 2>&1 &
 else
+    echo "--------- start $1 ----------"
     java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1
 fi
