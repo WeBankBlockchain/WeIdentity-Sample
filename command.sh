cbb385e89043e3796265462ee315f9c0ded4392b
yg3630536 <125480926@qq.com>
2019-07-22 10:46:28 +0800

    fix md and  script

--- command.sh
+++ command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-
+daemon_pid=
 if [[ "$1" != "issuer" ]];then
   if [[ "$1" != "user_agent" ]];then
      if [[ "$1" != "verifier" ]];then
@@ -11,9 +11,30 @@ if [[ "$1" != "issuer" ]];then
   fi
 fi
 
+getDaemonPid(){
+   daemon_pid=`ps aux|grep "DemoCommand" | grep "daemon" | grep -v grep|awk '{print $2}'|head -1` 
+}
+
 if [[ "$1" == daemon ]];then
+	getDaemonPid
+	if [ -n "$daemon_pid" ];then
+       echo "the AMOP server already start."
+       exit 1
+    fi
+    
     java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1 >/dev/null 2>&1 &
+    
+    sleep 2
+    
+    getDaemonPid
+    if [ -n "$daemon_pid" ];then
+       echo "the AMOP server start success."
+    else
+       echo "the AMOP server start fail."
+    fi
+   
 else
     echo "--------- start $1 ----------"
     java -cp dist/conf/:dist/lib/*:dist/app/* com.webank.weid.demo.command.DemoCommand $1
 fi
+
--- stop.sh
+++ stop.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ps aux | grep com.webank.weid.demo.server.SampleApp |  awk '{print $2}' | xargs kill -9
+sample_pid=
+
+getPid() {
+   sample_pid=`ps aux|grep "SampleApp" | grep -v grep|awk '{print $2}'|head -1` 
+}
+
+getPid;
+
+if [ -n "$sample_pid" ];then
+   kill -9 $sample_pid
+   echo "the server stop success."
+else 
+   echo "the server already stop."
+fi
